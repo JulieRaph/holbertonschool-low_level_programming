a804935da37cbacd73e3cab8ed76de5aaec95ad1
Julie Tolve <9919@holbertonstudents.com>
2024-10-23 09:32:51 +0200

    Print num with putchar

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0 (Success)
+*/
+
+int main(void)
+{
+	int x;
+
+	for (x = 48; x <= 57; x++)
+		putchar(x);
+
+	putchar('\n');
+	return (0);
+}
