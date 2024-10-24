d1714d00c9fa308e6b49a0059933debaac9d49f0
Julie Tolve <9919@holbertonstudents.com>
2024-10-24 12:46:00 +0200

    Print alphabet function

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function print alphabet
+ *
+ * Return: always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+
+		_putchar(i);
+
+	_putchar('\n');
+	return (0);
+}
