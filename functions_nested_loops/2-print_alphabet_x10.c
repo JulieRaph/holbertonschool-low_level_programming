396d19f924a9eae1623be4d48a844a18a88b3654
Julie Tolve <9919@holbertonstudents.com>
2024-10-24 13:45:00 +0200

    Print alphabet x10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function print_alphabet_x10
+ *
+ * Return: always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	i = 1;
+
+	while (i <= 10)
+	{
+		for (j = 'a'; j >= 'z'; j++)
+		{
+			_putchar('j');
+		}
+		_putchar('\n');
+		i++;
+	}
+}
