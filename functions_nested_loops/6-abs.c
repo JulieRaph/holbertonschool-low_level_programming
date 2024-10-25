f3936983f8f23c371c9d5186818dc9906e702f72
Julie Tolve <9919@holbertonstudents.com>
2024-10-25 10:52:42 +0200

    Fonction absolute calcule

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - compute absolute value of an integer
+ *
+ * @x: valeur pour absolute
+ *
+ * Return: return value _abs
+*/
+
+int _abs(int x)
+{
+	if (x >= 0)
+		return (x);
+	else
+		return (-x);
+	return (0);
+}
