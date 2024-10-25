0604be3c42af9087525254b2b3beb8cec5fc7be0
Julie Tolve <9919@holbertonstudents.com>
2024-10-25 15:55:33 +0200

    Multiplication table print

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * times_table - function multiplication table
+ *
+ * @x, r, c: variable resultat, raw, column of the multiplication's table
+ *
+ * Return: void return (nothing)
+ */
+
+void times_table(void)
+{
+	int x, r, c, m;
+
+	for (m = 1; m <= 9 ; m++)
+	{
+		for (r = 0 ; r <= 9 ; r++)
+		{
+			for (c = 0 ; c <= 9 ; c++)
+			{
+				x = r * c;
+
+				if (x <= 9)
+					_putchar(x + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				if (x > 9)
+
+					_putchar((x / 10) + '0');
+				_putchar((x % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+
+		}
+		_putchar('\n');
+	}
+}
