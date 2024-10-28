25c3cb731449247943607784da4981fbe99c8a9c
Julie Tolve <9919@holbertonstudents.com>
2024-10-28 10:37:58 +0100

    Addition int

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - add two integers and returns the result
+ * @b: entier en addition
+ * @c: entier en addition
+ * Return: always 0 (Success)
+*/
+
+int add(int b, int c)
+{
+	return (b + c);
+}
