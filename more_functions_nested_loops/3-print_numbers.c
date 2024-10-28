2fa8a3ea638d54445d7b60b25aac45c7555b4015
Julie Tolve <9919@holbertonstudents.com>
2024-10-28 13:31:00 +0100

    Print numbers

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print number between 0 9
+ *
+ * Return: void (nothing)
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 48 ; n <= 57 ; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
