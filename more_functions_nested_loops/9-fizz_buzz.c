4c03de99c6a65151add8e6f2a7fc3b99e3fe7e14
Julie Tolve <9919@holbertonstudents.com>
2024-10-29 13:37:49 +0100

    Print FizzBuzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * main - fonction principale
+ * i : parametre int
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	for (i = 1 ; i <= 100 ; i++)
+	{
+
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if (i == 100)
+		{
+			printf("Buzz");
+		}
+		else
+			printf("%d", i);
+	}
+	return (0);
+}
