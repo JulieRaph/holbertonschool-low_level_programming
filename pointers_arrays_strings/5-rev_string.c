654e4e36e1ae99cb829b8656a0f37f84bbe16702
Julie Tolve <9919@holbertonstudents.com>
2024-10-31 10:25:37 +0100

    Print reverse string

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse string
+ * @s: pointer string
+ * Return: void (nothing)
+ */
+
+void rev_string(char *s)
+{
+	char tmp;
+	int end = 0;
+	int start = 0;
+
+	while (s[end] != 0)
+	{
+		end++;
+	}
+	end--;
+
+	while (start < (end / 2))
+	{
+		tmp = s[start];
+		s[start] = s[end];
+		s[end] = tmp;
+
+		start++;
+		end--;
+	}
+}
