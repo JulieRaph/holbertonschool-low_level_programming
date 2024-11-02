98b55dcfe4c629d45b6ca457dee4450eb6def3f5
Julie Tolve <9919@holbertonstudents.com>
2024-11-02 17:44:44 +0100

    Function copy string for n int max char

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *_strncpy - copy two strings
+ * @dest: parameter pointer destination
+ * @src: parameter pointer source
+ * @n: integer
+ * Return: always 0 (Success)
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int x = 0;
+
+	while (x < n && *src != '\0')
+	{
+		*dest++ = *src;
+		x++;
+	}
+	while (x < n)
+	{
+		*dest++ = '\0';
+		x++;
+	}
+
+	return (dest);
+}
