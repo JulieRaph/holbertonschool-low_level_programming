1c2d6c08fc433e039b3dedc0ccc7a5aa800a9c14
Julie Tolve <9919@holbertonstudents.com>
2024-11-05 15:25:10 +0100

    Print diagonale principale and second

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -15,12 +15,12 @@ void print_diagsums(int *a, int size)
 
 	for (i = 0 ; i < size ; i++)
 	{
-		sum1 += a[i + size + i];
+		sum1 += a[i * size + i];
 	}
 
 	for (i = 0 ; i < size ; i++)
 	{
-		sum2 += a[i + size + (size - 1 - i)];
+		sum2 += a[i * size + (size - 1 - i)];
 	}
 	printf("%ld, %ld\n", sum1, sum2);
 }
