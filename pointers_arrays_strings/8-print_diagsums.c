8831f15a7b241588ec4b66ae6b12001918727443
Julie Tolve <9919@holbertonstudents.com>
2024-11-05 15:20:16 +0100

    Print diagonale principale and second

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_diagsums - print the sum of the two diagonals of a square
+ * @a: parameter pointer array
+ * @size: integer diagonale
+ * Return: void (nothing)
+*/
+
+void print_diagsums(int *a, int size)
+{
+	int i;
+	long int sum1 = 0;
+	long int sum2 = 0;
+
+	for (i = 0 ; i < size ; i++)
+	{
+		sum1 += a[i + size + i];
+	}
+
+	for (i = 0 ; i < size ; i++)
+	{
+		sum2 += a[i + size + (size - 1 - i)];
+	}
+	printf("%ld, %ld\n", sum1, sum2);
+}
