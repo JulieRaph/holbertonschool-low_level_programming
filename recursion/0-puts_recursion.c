3958b3efc75f18497a832884d01a199f0a57a87d
Julie Tolve <9919@holbertonstudents.com>
2024-11-06 09:48:55 +0100

    Print a string with recursion

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -4,12 +4,17 @@
  * _puts_recursion - print a string with new line
  * @s: parameter pointer string
  * Return: void (nothing)
-*/
+ */
 
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-		_putchar(s);
-	_puts_recursion(++s);
-
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+	_putchar('\n');
+	}
 }
