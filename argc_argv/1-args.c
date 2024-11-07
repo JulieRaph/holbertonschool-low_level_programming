d8c3e6e06cde340c5430c43c0478b20adcd52259
Julie Tolve <9919@holbertonstudents.com>
2024-11-07 10:24:18 +0100

    Print number of arguments

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -9,13 +9,9 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
+	(void)*argv;
 
 	printf("Nombre d'arguments : %d\n", argc);
 
-	for (i = 0 ; i < argc ; i++)
-	{
-		printf("Argument %d : %s\n", i, argv[i]);
-	}
 	return (0);
 }
