fa95efd0f61bec683fc9e13f41fc5b34c4870756
Julie Tolve <9919@holbertonstudents.com>
2024-11-15 16:42:19 +0100

    Concatenate two strings with malloc

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -10,40 +10,29 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *s3;
-	unsigned int i = 0;
+	unsigned int i;
 	unsigned int j = 0;
-	unsigned int size1;
-	unsigned int size2;
+	unsigned int l = 0;
 
-	for (size1 = 0; s1 && s1[size1]; size1++)
+	if (s1 == NULL)
+		s1 = "";
 
-		for (size2 = 0; s2 && s2[size2]; size2++)
+	if (s2 == NULL)
+		s2 = "";
 
-			s3 = malloc(sizeof(char) * (size1 + size2 + 1));
+	for (i = 0 ; s1[i] || s2[i] ; i++)
+		l++;
+
+	s3 = malloc(sizeof(char) * l);
 
 	if (s3 == NULL)
 		return (NULL);
 
-	if (s1)
-	{
-		while (i < size1)
-		{
-			s3[i] = s1[i];
-			i++;
-		}
-	}
+	for (i = 0 ; s1[i] ; i++)
+		s3[j++] = s1[i];
 
-	if (s2)
-	{
-		while (i < (size1 + size2))
-		{
-			s3[i] = s2[j];
-			i++;
-			j++;
-		}
-	}
-
-	s3[i] = '\0';
+	for (i = 0 ; s2[i] ; i++)
+		s3[j++] = s2[i];
 
 	return (s3);
 }
