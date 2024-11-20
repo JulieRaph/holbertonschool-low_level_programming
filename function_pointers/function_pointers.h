cad22515148ee2e1e5482cbc315a007d1ff1c892
Julie Tolve <9919@holbertonstudents.com>
2024-11-20 09:30:20 +0100

    Create a name

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,31 +1,12 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <stddef.h>
 #include <string.h>
-#include "dog.h"
 
-int _putchar(char c);
-void _puts_recursion(char *s);
-void _print_rev_recursion(char *s);
-int _strlen_recursion(char *s);
-int factorial(int n);
-int _pow_recursion(int x, int y);
-int _sqrt_recursion(int n);
-int is_prime_number(int n);
-char *create_array(unsigned int size, char c);
-char *_strdup(char *str);
-char *str_concat(char *s1, char *s2);
-int **alloc_grid(int width, int height);
-void free_grid(int **grid, int height);
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
