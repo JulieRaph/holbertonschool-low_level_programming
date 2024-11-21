f0af9c723d8ef59d4041621c644f85875f3c0c8d
Julie Tolve <9919@holbertonstudents.com>
2024-11-21 12:17:29 +0100

    Découverte de la calculatrice

---
+++ function_pointers/3-main.c
@@ -0,0 +1,42 @@
+#include "calc.h"
+
+/**
+ * main - fonction principale lien avec file pour calculatrice
+ * @argc: arguments
+ * @argv: nombres d'arguments
+ * Return: sum et 0 pour succes
+*/
+
+int main(int argc, char *argv[])
+{
+	int a, b, sum;
+	int (*op)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	op = get_op_func(argv[2]);
+
+	if (op == NULL || argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	sum = op(a, b);
+
+	if (b == 0 && (argv[2][0] == 37 || argv[2][0] == 47))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "calc.h"
+
+/**
+ * op_add - Function pour additionner des entiers
+ * @a: premier int
+ * @b: second int
+ * Return: always 0 (Success)
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Function pour soustraire  des entiers
+ * @a: premier int
+ * @b: second int
+ * Return: always 0 (Success)
+*/
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Function pour multiplier des entiers
+ * @a: premier int
+ * @b: second int
+ * Return: always 0 (Success)
+*/
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Function pour diviser des entiers
+ * @a: premier int
+ * @b: second int
+ * Return: always 0 (Success)
+*/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Function pour moduler des entiers
+ * @a: premier int
+ * @b: second int
+ * Return: always 0 (Success)
+*/
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
