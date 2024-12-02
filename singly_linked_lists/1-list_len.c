5a7e49bc488ff37ef43e860c4731e2a43eac5af2
Julie Tolve <9919@holbertonstudents.com>
2024-12-02 14:48:26 +0100

    Return numb of elements

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * list_len - function to return numbers of elements linked list
+ * @h: element head
+ * Return: numbers of elements 
+*/
+
+size_t list_len(const list_t *h)
+{
+	const list_t *current = h;
+	int i = 0;
+
+	if (current == NULL)
+		return (0);
+	
+	while (current)
+	{
+		i++;
+		current = current->next;
+	}
+	return (i);
+}
