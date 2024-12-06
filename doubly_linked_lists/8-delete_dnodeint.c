fb7c5a0fbe35d4eec6443b0f780ac5a3da9045f1
Julie Tolve <9919@holbertonstudents.com>
2024-12-06 11:05:49 +0100

    Delete a node at index into a list

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -32,7 +32,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 			current->next->prev = NULL;
 		}
 		free(current);
-		return (-1);
+		return (1);
 	}
 
 	if (current == NULL)
